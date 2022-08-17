61d86c153ea096d66918cac27d2eccd8a2413ce9
Alan F <68345864+AlanFletes@users.noreply.github.com>
2022-08-17 14:07:03 -0600

    Espada de cobre blando
    Se añadió la espada de cobre blando (Sin textura)

--- src/main/java/com/catpaw/coppamod/mess/ItemInit.java
+++ src/main/java/com/catpaw/coppamod/mess/ItemInit.java
@@ -7,8 +7,13 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ItemInit {
+    //Register
     public static final DeferredRegister<Item> itemRegister = DeferredRegister.create(ForgeRegistries.ITEMS, CoppaMod.MODID);
+    //Items
     public static final RegistryObject<Item> refinedCopperItem = itemRegister.register("refined_copper_item",()->new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+    //Armas
+    public static final RegistryObject<SwordItem> copperSword = itemRegister.register("copper_sword",()->new SwordItem(CoppaModTiers.COPPER,0,0,new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
+    //Herramientas
     public static final RegistryObject<PickaxeItem> copperPickaxe = itemRegister.register("copper_pickaxe",()->new PickaxeItem(CoppaModTiers.COPPER,0,0,new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
     //public static final RegistryObject<Item> ex_items = items.register("example",()->new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 }

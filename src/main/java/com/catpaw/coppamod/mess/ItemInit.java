b78a53937f4b60038d7afc8d753602e911da635e
Alan F <68345864+AlanFletes@users.noreply.github.com>
2022-08-17 15:25:14 -0600

    Corregido daño de espada de cobre blando

--- src/main/java/com/catpaw/coppamod/mess/ItemInit.java
+++ src/main/java/com/catpaw/coppamod/mess/ItemInit.java
@@ -11,9 +11,10 @@ public class ItemInit {
     public static final DeferredRegister<Item> itemRegister = DeferredRegister.create(ForgeRegistries.ITEMS, CoppaMod.MODID);
     //Items
     public static final RegistryObject<Item> refinedCopperItem = itemRegister.register("refined_copper_item",()->new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+    public static final RegistryObject<Item> compactCopperItem = itemRegister.register("compact_copper_item",()->new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
     //Armas
-    public static final RegistryObject<SwordItem> copperSword = itemRegister.register("copper_sword",()->new SwordItem(CoppaModTiers.COPPER,0,0,new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
+    public static final RegistryObject<SwordItem> copperSword = itemRegister.register("copper_sword",()->new SwordItem(CoppaModTiers.COPPER_SWORD,0,0,new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
     //Herramientas
-    public static final RegistryObject<PickaxeItem> copperPickaxe = itemRegister.register("copper_pickaxe",()->new PickaxeItem(CoppaModTiers.COPPER,0,0,new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
+    public static final RegistryObject<PickaxeItem> copperPickaxe = itemRegister.register("copper_pickaxe",()->new PickaxeItem(CoppaModTiers.COPPER_PICKAXE,0,0,new Item.Properties().tab(CreativeModeTab.TAB_TOOLS)));
     //public static final RegistryObject<Item> ex_items = items.register("example",()->new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 }

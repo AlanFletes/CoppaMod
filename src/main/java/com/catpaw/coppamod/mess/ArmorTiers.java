66e7e22f3566bb5652e98714c11948e0e2174aae
Alan F <68345864+AlanFletes@users.noreply.github.com>
2022-08-18 19:04:58 -0600

    Fin
    Finalizada la integración de todas las herramientas, armas, armaduras y otros objetos de cobre templado.

--- src/main/java/com/catpaw/coppamod/mess/ArmorTiers.java
+++ src/main/java/com/catpaw/coppamod/mess/ArmorTiers.java
@@ -9,4 +9,5 @@ import net.minecraftforge.client.event.ScreenEvent;
 
 public class ArmorTiers {
     public static final CopperArmorTiers COPPER = new CopperArmorTiers("COPPER",500,new int[]{20,14,15,10},20, SoundEvents.ARMOR_EQUIP_IRON,0,0, ()->Ingredient.of(new ItemStack(Items.COPPER_INGOT)));
+    public static final CopperArmorTiers TEMPLATED_COPPER = new CopperArmorTiers("TEMPLATED_COPPER",1000,new int[]{40,28,30,20},20,SoundEvents.ARMOR_EQUIP_DIAMOND,5,5,()->Ingredient.of(new ItemStack(ItemInit.templatedCopperIngot.get())));
 }
